7e0473d9e67473e1b5ff877f66d5440beb4bd6e0
Sean <seanvhussey@outlook.com>
2020-05-12 18:21:21 -0500

    commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function generateQuiz(
+  questions,
+  quizContainer,
+  resultsContainer,
+  submitButton
+) {}
+
+function answer {
+    if answer === correctAnswerindex
+    alert(correct!)
+}
+
+var myQuestions = [
+  {
+    question: "What tags do you use to insert JavaScript into a file?",
+    answers: {
+      a: "href",
+      b: "Java",
+      c: "link",
+      d: "script",
+    },
+    correctAnswer: "d",
+  },
+];
