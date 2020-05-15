db45b5df2e36efcf6e0c5465655ee205878a8269
Sean <seanvhussey@outlook.com>
2020-05-15 18:20:37 -0500

    commit readme

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
-function generateQuiz(
-  questions,
-  quizContainer,
-  resultsContainer,
-  submitButton
-) {}
-
-function answer {
-    if answer === correctAnswerindex
-    alert(correct!)
-}
-
-var myQuestions = [
+var questions = [
+  {
+    question: "What is an object that holds multiple values?",
+    choiceA: ["Array", "alphanumeric", "ordered list", "Objective list"],
+    correct: 0
+  },
   {
     question: "What tags do you use to insert JavaScript into a file?",
-    answers: {
-      a: "href",
-      b: "Java",
-      c: "link",
-      d: "script",
-    },
-    correctAnswer: "d",
+    choices = ["href", "Java", "link", "script"],
+    correct = 3
   },
 ];
+
+
+function generateQuiz(){
+  for (i = 0; i<questions.length; i++)
+};
+
+var questionsEl = document.getElementById("questionDisplay");
+var choiceEl = document.getElementById("choices");
+function showquestions() {
+  var questionTitle = document.getElementById("questionTitle");
+  var currentQuestion = questions[0];
+  var choices = choices[0];
+  questionTitle.innerHTML = currentQuestion.question;
+  console.log(currentQuestion.question);
+}
+showquestions();
